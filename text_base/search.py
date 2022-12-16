a8c5eb0ad18b783c20d82564e92fc1132a3e8fb2
Timofey Smirnov <timothyxp.mail.ru>
2022-12-16 14:36:50 +0300

    add comments

--- text_base/search.py
+++ text_base/search.py
@@ -2,6 +2,8 @@ from typing import List, Tuple, Dict, Optional
 from dataclasses import dataclass
 import pymorphy2
 import json
+from .utils import get_document
+from collections import Counter
 
 
 @dataclass
@@ -12,13 +14,15 @@ class SearchAnswer:
 class Searcher:
     tf_index: Dict[str, List[Tuple[int, int]]]
     idf_index: Dict[str, float]
+    top_terms: List[str]
 
     def __init__(self, base_path: str, index_path: str):
         self.create_structure()
 
     def create_structure(self):
         """
-        Создает индекс документов
+        Создает индекс документов путь до документа -> номер
+        поддерживает топ термы в top_terms
         """
         pass
 
