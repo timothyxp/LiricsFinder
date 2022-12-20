51ef1021ed6473577ef7ec8dd4fedb6db1cb3564
InfiniteTears <lvp.50@yandex.ru>
2022-12-20 14:24:45 +0300

    add user_story_database

--- database/database.py
+++ database/database.py
@@ -1,12 +1,20 @@
+from tinydb import TinyDB, Query
+from datetime import datetime
+
+
 class DataBase:
     def __init__(self, database_path: str):
-        pass
+        self.db = TinyDB(database_path)
+        self.user = Query()
 
     def save_log(self, user_id, chat_id, text, is_audio, is_file, command, result):
-        pass
+        dt = datetime.now()
+        dt_str = datetime.strptime(str(dt)[:-7], '%Y-%m-%d %H:%M:%S')
+        self.db.insert({'user_id': user_id, 'chat_id': chat_id, 'text': text, 'is_audio': is_audio, 'is_file': is_file,\
+                        'command': command, 'result': result, 'date_time': dt_str})
 
-    def get_user_story(self, user_id, chat_id):
-        pass
+    def get_user_story(self, user_id):
+        return self.db.search(self.user.user_id == user_id)
 
     def get_user_last_query(self, user_id, chat_id):
-        pass
+        return self.db.search(self.user.user_id == user_id and self.user.chat_id == chat_id)[-1]
